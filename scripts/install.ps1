fd9d9101e5a304a0b26dc6c22cde046331411e33
mawa <ahmadfarhankholik01@gmail.com>
2026-08-21 13:54:47 +0700

    chore(release): bump version to v0.1.25

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -11,7 +11,7 @@ $ErrorActionPreference = 'Stop'
 
 $Repo = "plumpslabs/bro-code"
 $BinaryName = "brocode"
-$DefaultTag = "v0.1.24"
+$DefaultTag = "v0.1.25"
 
 # 1. Detect Architecture
 $Arch = $env:PROCESSOR_ARCHITECTURE
